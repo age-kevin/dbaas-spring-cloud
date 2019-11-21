e4043061f9343ce41868b120902022178e135d45
age <404697918@qq.com>
2019-11-21 22:47:12 +0800

    update by AGE

--- dbaas-eureka-server-7003/src/main/java/com/age/dbaas/DbaasEurekaServer7003Application.java
+++ dbaas-eureka-server-7003/src/main/java/com/age/dbaas/DbaasEurekaServer7003Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DbaasEurekaServer7003Application {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(DbaasEurekaServer7003Application.class, args);
     }
