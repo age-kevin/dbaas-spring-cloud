9bd8298b4bed7629391bef728b7d1a4cae98ef0a
age <404697918@qq.com>
2019-11-26 21:03:44 +0800

    update by AGE

--- dbaas-redis-service/src/main/java/com/age/dbaas/DbaasRedisServiceApplication.java
+++ dbaas-redis-service/src/main/java/com/age/dbaas/DbaasRedisServiceApplication.java
@@ -2,8 +2,10 @@ package com.age.dbaas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DbaasRedisServiceApplication {
         public static void main(String[] args) {
         SpringApplication.run(DbaasRedisServiceApplication.class, args);
